a1b5be5496f67324ea135dec5a770ee3f63e7e30
rbensua <rbensua@gmail.com>
2015-12-18 10:31:35 +0100

    ...

--- R/afmContactPoint.R
+++ R/afmContactPoint.R
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 #' @title Contact point
 #' 
 #' @description 
-=======
->>>>>>> 05e3141ced9de0ca59a9494352758e57a829facc
 #' Find the contact point in for the Force-Distance curve
 #' following the method described in Microscopy Research and Technique 2013
 #' 
@@ -12,7 +9,6 @@
 #' @param mul1 First multiplier for the first alarm threshold
 #' @param mul2 Second multiplier for the second alarm threshold  
 #' @param Delta Logical. If TRUE, then the statistic for determining the contact point is the differences between two consecutive values of the slope of the local regression line. If FALSE then the slope itself is used.
-<<<<<<< HEAD
 #' @return A list of: 
 #' 
 #' \code{CP} The contact point value. 
@@ -31,16 +27,6 @@ afmContactPoint <- function(afmdata,width=1,mul1,mul2, lagdiff = width,
   data.approach <- subset(afmdata$data, Segment == "approach")
   Z <- data.approach$Z
   Force <- data.approach$Force
-=======
-#' @return A list of: CP The contact point value. iCP The position in the array for the contact point value. delta The delta signal. noise The noise of the delta signal
-
-
-
-afmContactPoint <- function(afmdata,width=1,mul1,mul2,Delta=TRUE){
-  data.approach <- subset(afmdata$data, Segment == "approach")
-  Z <- data.approach$Z
-  Force <- data.approach$F
->>>>>>> 05e3141ced9de0ca59a9494352758e57a829facc
   n = length(Z)
   if(length(Force)!=n){
     stop("Z and Force must be the same length")
@@ -52,13 +38,8 @@ afmContactPoint <- function(afmdata,width=1,mul1,mul2,Delta=TRUE){
  # bRoll <- rollapply(app,width,FUN = function(X) 
   #  coef(lm.fit(X[,1:2],X[,3]))[2],by.column=FALSE,align="right")
  bRoll <- windowedFit(app,width) 
-<<<<<<< HEAD
  delta<- diff(bRoll, lag = lagdiff)
  delta <- c(rep(0,width+lagdiff),delta,rep(0,width))
-=======
- delta<- diff(bRoll)
- delta <- c(rep(0,width+1),delta,rep(0,width))
->>>>>>> 05e3141ced9de0ca59a9494352758e57a829facc
   if (!Delta){
     delta <- c(rep(0,width),bRoll,rep(0,width))
   }
@@ -74,11 +55,7 @@ afmContactPoint <- function(afmdata,width=1,mul1,mul2,Delta=TRUE){
     return(list(CP=NA,iCP=NA,delta=delta,noise=noise))
 
   } else{
-<<<<<<< HEAD
     j <- max(idxSmTol1[idxSmTol1<min(idxGrTol2)])#+1  
-=======
-    j <- max(idxSmTol1[idxSmTol1<min(idxGrTol2)])+1  
->>>>>>> 05e3141ced9de0ca59a9494352758e57a829facc
   
   if ((j > 1) & (delta[j] != 0)){
     eps <- (tol1-abs(delta[j]))/abs(delta[j+1]-delta[j]) # factor de proporcionalidad entre 0 y 1    
