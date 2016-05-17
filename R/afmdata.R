47732c97e122878f16c73740f8fadd311947cfcd
rbensua <rbensua@gmail.com>
2016-05-17 10:42:47 +0200

    documenting...

--- R/afmdata.R
+++ R/afmdata.R
@@ -19,7 +19,16 @@
 #'
 #' @return An object of class \code{afmdata}
 #'
-
+#'@examples
+#'#Making some artifical data following a L-J 12-6 potential
+#'n <- 1000
+#'z <- seq(from = 9e-3, to = 1e-1, length.out = n )
+#'u0 <- 1e-5
+#'z0 <- 1e-2
+#'Force <- -u0*(12*z0^6/z^7-12*z0^12/z^13)
+#'Segment <- rep("approach",n)
+#'AFMcurve <- afmdata(data.frame(Z = z, Force = Force, Segment  = Segment))
+#'plot(AFMcurve)
 
 afmdata <-
   function(data, dstr = "Z", Fstr = "Force", Segstr = "Segment", 
