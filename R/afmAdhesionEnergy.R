518cf5e0513ac1bc620c2b886ddbc7f5a2064c0e
rbensua <rbensua@gmail.com>
2016-06-15 13:41:20 +0200

    Trying to comply with R CMD check --as-cran

--- R/afmAdhesionEnergy.R
+++ R/afmAdhesionEnergy.R
@@ -1,3 +1,40 @@
+#' @title Adhesion Energy
+#'
+#' @description
+#' Finds the adhesion and the full detach energies from the retract segment of the AFM 
+#' F-d curve. 
+#' @usage afmAdhesionEnergy(afmdata, width = 1, lagdiff = width, mul, mdj = NULL)
+#' @param \code{afmdata} An afmdata or afmexperiment class variables. Baseline correction should
+#'  have been done already.
+#' @param \code{width} Width of the window for the local regression (in vector position units)
+#' @param \code{lagdiff} Lag for estimating the differences in Delta (or slopes) signal. 
+#' By default it takes the same value as the window with.
+#' @param \code{mul} Multiplier for the calculating the threshold inthe estimation of jumps 
+#' and peaks in the Delta signal
+#' @param \code{mdj} Minimum distance between jumps. If none is given then it will be set equal 
+#' to \code{width}
+#' @return An \code{afmdata} class variable which will consist on the original 
+#' input \code{afmdata} variable plus a new list named \code{AdhEner} with the 
+#' following fields:
+#'
+#' \code{Points} Array containing the indices of the retract segment where the adhesion begins, 
+#' the unbinding event takes place and the adhesion ends.
+#'
+#' \code{Energies} Data frame with three columns: \code{E1adh}, \code{E2adh} and \code{Etotal}, 
+#' being the first one the energy from the begining of the adhesion until the unbinding event,
+#' then second one the energy from the unbinding event until the full detachment of the 
+#' tip, and the third one, the sum of them.
+#' @examples
+#' path <- path.package("afmToolkit")
+#' data <- afmReadJPK("force-save-JPK-3h.txt", path = path)
+#' data <- afmContactPoint(data, width = 20, mul1 = 1, mul2 = 10)
+#' data <- afmDetachPoint(data, width = 20, mul1 = 2, mul2 = 30)
+#' data <- afmBaselineCorrection(data)
+#' data <- afmAdhesionEnergy(data, width = 20, mul = 10)
+#' str(data$AdhEner)
+#' @importFrom stats sd
+#' @importFrom utils tail
+#' @export
 afmAdhesionEnergy <-
   function(afmdata,
            width = 1,
@@ -7,7 +44,7 @@ afmAdhesionEnergy <-
     # Extract the retract segment from the afmdata
     if (is.afmexperiment(afmdata)) {
       afmexperiment <-
-        lapply(afmdata, function(x){
+        lapply(afmdata, function(x) {
           if (!is.null(x$params$curvename)) {
             print(paste("Processing curve: ", x$params$curvename), sep = " ")
           }
@@ -17,8 +54,8 @@ afmAdhesionEnergy <-
             mul = mul,
             lagdiff = lagdiff,
             mdj = mdj
-          )}
-    )
+          )
+        })
       return(afmexperiment(afmexperiment))
     } else if (is.afmdata(afmdata)) {
       data <- subset(afmdata$data, Segment == "retract")
@@ -46,33 +83,35 @@ afmAdhesionEnergy <-
       delta <- c(rep(0, width + lagdiff), delta, rep(0, width))
       bRoll <- c(rep(0, width), bRoll, rep(0, width))
       
-      #Noise at the begining of the curve
-      noiseEndDelta <-
-        sd(delta[width:(width + as.integer(0.1 * n))], na.rm =
-             TRUE)
-      # Noise at the end of the curve
-      noiseBeginDelta <-
+      #Noise at the contact part of the curve
+#      noiseContactDelta <-
+#        sd(delta[width:(width + as.integer(0.1 * n))], na.rm =
+#             TRUE)
+
+            # Noise at the non-contact part of the curve
+      noiseFreeDelta <-
         sd(delta[(n - (width + as.integer(0.1 * n))):(n - width)],
            na.rm = TRUE)
       # Tolerances in Delta
       
-      tol2Begin <- mul * noiseBeginDelta
-      tol2End <- mul * noiseEndDelta
+      tolFree <- mul * noiseFreeDelta
+ #     tolContact <- mul * noiseContactDelta
       
       # Jumps in Delta
       jumpsDelta <- list()
       
       # Indices above the threshold (in the slope)
-      idxGrTol2Begin <- which(abs(delta) > tol2Begin)
-      idxGrTol2Begin <- idxGrTol2Begin[idxGrTol2Begin <= imax + width &
-                                         idxGrTol2Begin >= which.min(Force)]
+      idxGrTolFree <- which(abs(delta) > tolFree)
+      idxGrTolFree <- idxGrTolFree[idxGrTolFree <= imax + width &
+                                       idxGrTolFree >= which.min(Force)]
       # Compute the number of jumps at the beginning
       
-      Njumps <- sum(diff(idxGrTol2Begin) > mdj) + 1
-      idxgr <- which(diff(idxGrTol2Begin) > mdj)
+      Njumps <- sum(diff(idxGrTolFree) > mdj) + 1
+      idxgr <- which(diff(idxGrTolFree) > mdj)
       
-      gr.begin <- c(idxGrTol2Begin[1], idxGrTol2Begin[idxgr + 1])
-      gr.end <- c(idxGrTol2Begin[idxgr], tail(idxGrTol2Begin, n = 1))
+      gr.begin <- c(idxGrTolFree[1], idxGrTolFree[idxgr + 1])
+      gr.end <-
+        c(idxGrTolFree[idxgr], tail(idxGrTolFree, n = 1))
       
       
       for (k in 1:Njumps) {
@@ -82,11 +121,9 @@ afmAdhesionEnergy <-
       
       
       
-      # Magnitudes of the jumps (in 2 ways)
+      # Magnitudes of the jumps
       
-      magnitude1 <- numeric() # Computing the range
-      magnitude1Corrected <- numeric()
-      magnitude2 <- numeric() # Integrating bRoll
+      
       magnitude2Corrected <- numeric()
       noiseForce <-
         sd(Force[(n - (width + as.integer(0.1 * n))):(n - width)],
@@ -102,34 +139,18 @@ afmAdhesionEnergy <-
           as.integer(jumpsDelta[[i]][1] + tail(jumpsDelta[[i]], n = 1)) /
           2 - lagdiff / 2
         pmid[i] <- pmedio
-        windowJump <- round(pmedio - radius2):round(pmedio + radius2)
-        magnitude1[i] <-
-          diff(range(Force[(pmedio - radius1):(pmedio + radius1)])) -
-          2 * (sqrt(2 * log(width)) - 0.588) * noiseForce
-        
-        magnitude1Corrected[i] <-
-          diff(range(Force[(pmedio - radius1):(pmedio + radius1)] -
-                       (bRoll[round(pmedio + radius2)] +
-                          bRoll[round(pmedio - radius2)]) / 2 *
-                       Z[(pmedio - radius1):(pmedio + radius1)])) -
-          2 * (sqrt(2 * log(width)) - 0.588) * noiseForce
-        
+        windowJump <-
+          round(pmedio - radius2):round(pmedio + radius2)
         zWJ <- Z[windowJump]
         bWJCorrected <-
           bRoll[windowJump] - (bRoll[round(pmedio + radius2)] +
                                  bRoll[round(pmedio - radius2)]) / 2
-        magnitude2[i] <- trapz(zWJ, bRoll[windowJump])
         magnitude2Corrected[i] <- trapz(zWJ, bWJCorrected)
       }
-      magnitudeDF <- data.frame(
-        pmid = pmid,
-        magnitude1 = magnitude1,
-        magnitude1Corrected = magnitude1Corrected,
-        magnitude2 = magnitude2,
-        magnitude2Corrected = magnitude2Corrected
-      )
+      magnitudeDF <- data.frame(pmid = pmid,
+                                magnitude = magnitude2Corrected)
       
-      bigJump <- which.max(magnitudeDF$magnitude2Corrected)
+      bigJump <- which.max(magnitudeDF$magnitude)
       pmidbigJump <- pmid[bigJump]
       leftPoint <-
         pmidbigJump + max(0.5 * (length(jumpsDelta[[bigJump]]) - lagdiff), width)
@@ -156,20 +177,14 @@ afmAdhesionEnergy <-
       EnerFun <- Eadh / Eadh[length(Eadh)]
       EnerFun[1] <- 0
       AdhEner <-  list(
-        Magnitudes = magnitudeDF,
         Points = c(imin, leftPoint, imax),
-        delta = delta,
-        noiseB = noiseBeginDelta,
-        noiseE = noiseEndDelta,
-        EnerFun = data.frame(Zrel = Zadh - max(Zadh), EnerFun = EnerFun),
         Energies = data.frame(
-          E1adh = E1adh,
-          E2adh = E2adh,
-          Weight = Weight
-        ),
-        jumpsDelta = jumpsDelta
+          E1adh = abs(E1adh),
+          E2adh = abs(E2adh),
+          Etotal = Weight
+        )
       )
-      return(append.afmdata(afmdata,AdhEner))
+      return(append.afmdata(afmdata, AdhEner))
     } else{
       stop("No afmdata or afmexperiment class input provided.")
     }
